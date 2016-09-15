6812034550ac7a2e746956722a5f75bdb2db2baa
= <lcf33123@foxmail.com>
2016-09-16 00:44:46 +0800

    学习react，写demo

--- requireJS初涉/js/a.js
+++ requireJS初涉/js/a.js
@@ -1,9 +1,20 @@
 define(['jquery'], function($) {
-	$(document).ready(function(){
-    	fun2();
-    });
+    // $(document).ready(function(){
+    //    	fun2();
+    //    });
 
-	function fun2(){
-		alert("fun222222222");
-	}
+    // function fun2(){
+    // 	alert("fun222222222");
+    // }
+
+    var module = {},
+        urls = { update: 'fadf' };
+    module.show = function() {
+        alert("show method");
+    }
+
+    return {
+        module: module,
+        urls: urls
+    }
 });
