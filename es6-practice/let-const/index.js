09faff689f02329852aaf860676368ce12cbd377
= <lcf33123@foxmail.com>
2016-11-05 15:53:12 +0800

    es6 let const promise

---
+++ es6-practice/let-const/index.js
@@ -0,0 +1,20 @@
+'use strict'; 
+
+{
+    let a = 10;
+    var b = 1;
+}
+console.log(a); // Uncaught ReferenceError: a is not defined
+console.log(b);
+
+function demo1(argument) {
+    var a = [];
+    for (var i = 0; i < 10; i++) {
+        a[i] = function() {
+            console.log(i);
+        }
+    } 
+    a[6](); // var命令 存在变量提升。let不存在变量提升。
+}
+
+demo1();
