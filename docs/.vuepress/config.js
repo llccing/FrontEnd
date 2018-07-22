1708005e1550f01a9c11a7dfc63b68d1b76821c8
llccing <llccing@llccingdeMBP.lan>
2018-07-22 23:12:47 +0800

    使用vuepress改造项目

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,28 @@
+module.exports = {
+    title: '千里',
+    description: 'just do it! ',
+    base: '/frontEnd/',
+    serviceWorker: true,
+    // theme: 'vue',
+    themeConfig: {
+        nav: [{
+            text: 'Home', link: '/',
+        }, {
+            text: 'Knowledge', link: '/knowledge/',
+        }, {
+            text: 'Read', link: '/read/',
+        }, {
+            text: 'About', link: '/about/'
+        }],
+        sidebar: {
+            '/knowledge/': ['', 'JS-basic']
+        }
+    },
+    configureWebpack: {
+        resolve: {
+            alias: {
+                '@img': 'assets'
+            }
+        }
+    }
+}
---
+++ docs/README.md
@@ -0,0 +1,26 @@
+---
+home: true
+# heroImage: /frontEnd/hongkong_s.jpg
+actionText: 做一个有信仰的程序员 →
+actionLink: /about/
+features:
+- title: 知识点实践
+  details: 对于各种基础知识点（JS、HTML、CSS）的实践，总结。
+- title: 各种框架学习
+  details: 学习各种框架的细节，总结常用框架的最佳实践。
+- title: 业内信息
+  details: 前端领域内的最新消息，了解趋势。
+
+footer: llccing Copyright © 2018
+---
+
+
+# 推荐文章
+
+* [?](/about/) 编写简单代码的系列好帖，必读！
+* [ajax跨域完全详解视频](https://www.imooc.com/learn/947) 由浅入深，抽丝剥茧，好评如潮
+* [Springboot2 Webflux学习路径](https://www.imooc.com/article/27181) 响应式编程的前置基础讲解好文！
+
+
+
+
