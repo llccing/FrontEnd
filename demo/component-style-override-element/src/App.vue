21a8a4836858b67bff1f881f6f6805a2d3e1d134
liuchunfeng <liuchunfeng@jd.com>
2019-02-25 19:43:22 +0800

    feature element-ui table

--- demo/component-style-override-element/src/App.vue
+++ demo/component-style-override-element/src/App.vue
@@ -2,18 +2,59 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <form-compo style="width: 800px; margin: 0 auto;"></form-compo>
+    <big-table :data="data">
+       <el-table-column
+        prop="date"
+        label="日期"
+        width="180">
+      </el-table-column>
+      <el-table-column
+        prop="name"
+        label="姓名"
+        width="180">
+      </el-table-column>
+      <el-table-column
+        prop="address"
+        label="地址">
+      </el-table-column>
+    </big-table>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
 import formCompo from './components/formCompo.vue'
+import BigTable from './components/BigTable.vue'
 
 export default {
   name: 'app',
   components: {
     HelloWorld,
     formCompo,
+    BigTable,
+  },
+  data() {
+    return {
+      data: [
+        {
+            date: '2016-05-02',
+            name: '王小虎',
+            address: '上海市普陀区金沙江路 1518 弄'
+          }, {
+            date: '2016-05-04',
+            name: '王小虎',
+            address: '上海市普陀区金沙江路 1517 弄'
+          }, {
+            date: '2016-05-01',
+            name: '王小虎',
+            address: '上海市普陀区金沙江路 1519 弄'
+          }, {
+            date: '2016-05-03',
+            name: '王小虎',
+            address: '上海市普陀区金沙江路 1516 弄'
+          }
+      ]
+    };
   }
 }
 </script>
---
+++ demo/component-style-override-element/src/components/BigTable.vue
@@ -0,0 +1,224 @@
+<template>
+  <div class="el-table"
+    :class="[{
+      'el-table--fit': fit,
+      'el-table--striped': stripe,
+      'el-table--border': border || isGroup,
+      'el-table--hidden': isHidden,
+      'el-table--group': isGroup,
+      'el-table--fluid-height': maxHeight,
+      'el-table--scrollable-x': layout.scrollX,
+      'el-table--scrollable-y': layout.scrollY,
+      'el-table--enable-row-hover': !store.states.isComplex,
+      'el-table--enable-row-transition': (store.states.data || []).length !== 0 && (store.states.data || []).length < 100
+    }, tableSize ? `el-table--${ tableSize }` : '']"
+    @mouseleave="handleMouseLeave($event)">
+    <div class="hidden-columns" ref="hiddenColumns"><slot></slot></div>
+    <div
+      v-if="showHeader"
+      v-mousewheel="handleHeaderFooterMousewheel"
+      class="el-table__header-wrapper"
+      ref="headerWrapper">
+      <table-header
+        ref="tableHeader"
+        :store="store"
+        :border="border"
+        :default-sort="defaultSort"
+        :style="{
+          width: layout.bodyWidth ? layout.bodyWidth + 'px' : ''
+        }">
+      </table-header>
+    </div>
+    <div
+      class="el-table__body-wrapper"
+      ref="bodyWrapper"
+      :class="[layout.scrollX ? `is-scrolling-${scrollPosition}` : 'is-scrolling-none']"
+      :style="[bodyHeight]">
+      <table-body
+        :context="context"
+        :store="store"
+        :stripe="stripe"
+        :row-class-name="rowClassName"
+        :row-style="rowStyle"
+        :highlight="highlightCurrentRow"
+        :style="{
+           width: bodyWidth
+        }">
+      </table-body>
+      <div
+        v-if="!data || data.length === 0"
+        class="el-table__empty-block"
+        ref="emptyBlock"
+        :style="{
+          width: bodyWidth
+        }">
+        <span class="el-table__empty-text">
+          <slot name="empty">{{ emptyText || t('el.table.emptyText') }}</slot>
+        </span>
+      </div>
+      <div
+        v-if="$slots.append"
+        class="el-table__append-wrapper"
+        ref="appendWrapper">
+        <slot name="append"></slot>
+      </div>
+    </div>
+    <div
+      v-if="showSummary"
+      v-show="data && data.length > 0"
+      v-mousewheel="handleHeaderFooterMousewheel"
+      class="el-table__footer-wrapper"
+      ref="footerWrapper">
+      <table-footer
+        :store="store"
+        :border="border"
+        :sum-text="sumText || t('el.table.sumText')"
+        :summary-method="summaryMethod"
+        :default-sort="defaultSort"
+        :style="{
+          width: layout.bodyWidth ? layout.bodyWidth + 'px' : ''
+        }">
+      </table-footer>
+    </div>
+    <div
+      v-if="fixedColumns.length > 0"
+      v-mousewheel="handleFixedMousewheel"
+      class="el-table__fixed"
+      ref="fixedWrapper"
+      :style="[{
+        width: layout.fixedWidth ? layout.fixedWidth + 'px' : ''
+      },
+      fixedHeight]">
+      <div
+        v-if="showHeader"
+        class="el-table__fixed-header-wrapper"
+        ref="fixedHeaderWrapper" >
+        <table-header
+          ref="fixedTableHeader"
+          fixed="left"
+          :border="border"
+          :store="store"
+          :style="{
+            width: bodyWidth
+          }"></table-header>
+      </div>
+      <div
+        class="el-table__fixed-body-wrapper"
+        ref="fixedBodyWrapper"
+        :style="[{
+          top: layout.headerHeight + 'px'
+        },
+        fixedBodyHeight]">
+        <table-body
+          fixed="left"
+          :store="store"
+          :stripe="stripe"
+          :highlight="highlightCurrentRow"
+          :row-class-name="rowClassName"
+          :row-style="rowStyle"
+          :style="{
+            width: bodyWidth
+          }">
+        </table-body>
+        <div
+          v-if="$slots.append"
+          class="el-table__append-gutter"
+          :style="{
+            height: layout.appendHeight + 'px'
+          }"></div>
+      </div>
+      <div
+        v-if="showSummary"
+        v-show="data && data.length > 0"
+        class="el-table__fixed-footer-wrapper"
+        ref="fixedFooterWrapper">
+        <table-footer
+          fixed="left"
+          :border="border"
+          :sum-text="sumText || t('el.table.sumText')"
+          :summary-method="summaryMethod"
+          :store="store"
+          :style="{
+            width: bodyWidth
+          }"></table-footer>
+      </div>
+    </div>
+    <div
+      v-if="rightFixedColumns.length > 0"
+      v-mousewheel="handleFixedMousewheel"
+      class="el-table__fixed-right"
+      ref="rightFixedWrapper"
+      :style="[{
+        width: layout.rightFixedWidth ? layout.rightFixedWidth + 'px' : '',
+        right: layout.scrollY ? (border ? layout.gutterWidth : (layout.gutterWidth || 0)) + 'px' : ''
+      },
+      fixedHeight]">
+      <div v-if="showHeader"
+        class="el-table__fixed-header-wrapper"
+        ref="rightFixedHeaderWrapper">
+        <table-header
+          ref="rightFixedTableHeader"
+          fixed="right"
+          :border="border"
+          :store="store"
+          :style="{
+            width: bodyWidth
+          }"></table-header>
+      </div>
+      <div
+        class="el-table__fixed-body-wrapper"
+        ref="rightFixedBodyWrapper"
+        :style="[{
+          top: layout.headerHeight + 'px'
+        },
+        fixedBodyHeight]">
+        <table-body
+          fixed="right"
+          :store="store"
+          :stripe="stripe"
+          :row-class-name="rowClassName"
+          :row-style="rowStyle"
+          :highlight="highlightCurrentRow"
+          :style="{
+            width: bodyWidth
+          }">
+        </table-body>
+      </div>
+      <div
+        v-if="showSummary"
+        v-show="data && data.length > 0"
+        class="el-table__fixed-footer-wrapper"
+        ref="rightFixedFooterWrapper">
+        <table-footer
+          fixed="right"
+          :border="border"
+          :sum-text="sumText || t('el.table.sumText')"
+          :summary-method="summaryMethod"
+          :store="store"
+          :style="{
+            width: bodyWidth
+          }"></table-footer>
+      </div>
+    </div>
+    <div
+      v-if="rightFixedColumns.length > 0"
+      class="el-table__fixed-right-patch"
+      ref="rightFixedPatch"
+      :style="{
+        width: layout.scrollY ? layout.gutterWidth + 'px' : '0',
+        height: layout.headerHeight + 'px'
+      }"></div>
+    <div class="el-table__column-resize-proxy" ref="resizeProxy" v-show="resizeProxyVisible"></div>
+  </div>
+</template>
+<script>
+
+import {Table} from 'element-ui'
+
+export default {
+  mixins:[Table],
+    mounted () {
+        console.log('do what you want to do in mounted!')
+    },
+}
+</script>
