28bd6f3e725d4de9e7e1a96610e3a1fe4d1ce4b3
llccing <lcf33123@foxmail.com>
2019-09-28 18:44:47 +0800

    update 调整展开项

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,64 +1,3 @@
-const blogEcmascript = {
-  title: 'ES2015',
-  children: [
-    '',
-    // 'Async',
-    'let-const',
-    'function-extension',
-    'Generator-async',
-    'Generator',
-    'Iterator-for...of',
-    'number-extension',
-    'object-extension',
-    'Promise',
-    'Proxy-Reflect',
-    'Set-Map',
-    'String-extension',
-    'Symbol',
-    'variable-destructor-assignment',
-  ].map((item) => `./ECMAScript6/${item}`),
-};
-
-const blogThreejs = {
-  title: 'threejs journey',
-  children: ['00-start', '01-hello-world', '02-basic-component', '03-light', '04-material', '05-geometry', '06-high-geometry', '07-praticle', '08-import-high-geometry', 'webgl'].map(
-    (item) => `./threejs/${item}/`,
-  ),
-};
-
-const blogVue = {
-  title: 'vue 源码被解读',
-  children: [
-    '00-vue',
-    '01-flow',
-    '02-src-structure',
-    '03-compile',
-    '04-entry',
-    '10-data-driven',
-    '11-new-Vue',
-    '12-vue-mounted',
-    '13-render',
-    '14-virtual-DOM',
-    '15-createElement',
-    '16-update',
-    '20-components',
-  ].map((item) => `./vue-analysis/${item}`),
-};
-
-const blogReact = {
-  title: 'react journey',
-  children: ['00-react', '01-start-project'].map((item) => {
-    return `./react/${item}`;
-  }),
-};
-
-const blogVuepress = {
-  title: 'vuepress',
-  children: ['01-remark'].map((item) => `./vuepress/${item}`),
-};
-
-const blogOther = ['for', 'why', 'vueComponent'].map((item) => `./other/${item}`);
-
 module.exports = {
   title: '千里',
   description: 'just do it! ',
@@ -76,29 +15,106 @@ module.exports = {
       { text: 'About', link: '/about/' },
     ],
     sidebar: {
-      '/blog/': ['', ...blogOther],
+      '/blog/': ['', ...['for', 'why', 'vueComponent'].map(item => `./other/${item}`)],
       '/read/': [
         // ''空字符串表示当前目录的README文件
         '',
         {
           title: 'CSS世界',
+          collapsable: false,
           children: ['./css-world/01-overview', './css-world/02-terminology-concept'],
         },
         {
           title: '深入浅出Webpack',
-          children: ['./webpack/', './webpack/01']
-        }
+          collapsable: false,
+          children: ['./webpack/', './webpack/01'],
+        },
       ],
       '/css/': ['', 'flex'],
       '/js/': [
         '',
-        blogEcmascript,
+        {
+          title: 'ES2015',
+          collapsable: false,
+          children: [
+            '',
+            // 'Async',
+            'let-const',
+            'function-extension',
+            'Generator-async',
+            'Generator',
+            'Iterator-for...of',
+            'number-extension',
+            'object-extension',
+            'Promise',
+            'Proxy-Reflect',
+            'Set-Map',
+            'String-extension',
+            'Symbol',
+            'variable-destructor-assignment',
+          ].map(item => `./ECMAScript6/${item}`),
+        },
         {
           title: 'basic',
+          collapsable: false,
           children: ['./basic/apply-call'],
         },
       ],
-      '/lib/': ['', blogVue, blogThreejs, blogReact, blogVuepress, { title: 'egg', children: ['./egg/init'] }],
+      '/lib/': [
+        '',
+        {
+          title: 'vue 源码被解读',
+          collapsable: false,
+          children: [
+            '00-vue',
+            '01-flow',
+            '02-src-structure',
+            '03-compile',
+            '04-entry',
+            '10-data-driven',
+            '11-new-Vue',
+            '12-vue-mounted',
+            '13-render',
+            '14-virtual-DOM',
+            '15-createElement',
+            '16-update',
+            '20-components',
+          ].map(item => `./vue-analysis/${item}`),
+        },
+        {
+          title: 'threejs journey',
+          collapsable: false,
+          children: [
+            '00-start',
+            '01-hello-world',
+            '02-basic-component',
+            '03-light',
+            '04-material',
+            '05-geometry',
+            '06-high-geometry',
+            '07-praticle',
+            '08-import-high-geometry',
+            'webgl',
+          ].map(item => `./threejs/${item}/`),
+        },
+        {
+          title: 'react journey',
+          collapsable: false,
+          children: ['00-react', '01-start-project'].map(item => {
+            return `./react/${item}`
+          }),
+        },
+        {
+          title: 'vuepress',
+          collapsable: false,
+          children: ['01-remark'].map(item => `./vuepress/${item}`),
+        },
+        {
+          title: 'egg',
+          collapsable: false,
+          children: ['./egg/init']
+        },
+      ],
     },
   },
   configureWebpack: {
@@ -108,4 +124,4 @@ module.exports = {
       },
     },
   },
-};
+}
--- docs/lib/readme.md
+++ docs/lib/readme.md
@@ -4,8 +4,23 @@
 前端轮子数不胜数，在众多轮子中选出能够跑的快的，也是有很多参考指标的：
 GitHub的star数目，同事的评价，自己的使用体验等等。合理的使用框架会提高开发效率，在现在的项目进度要求下，是没有时间来重新造轮子的，所以选择框架成为了必须做的事情，如果你能选择前端、后端各种框架，那么你就是架构师。好的架构师就是尝试了各种轮子之后，善于总结并且能够形成自己的方法论的人。
 
-## 我用过的框架罗列
+## now
+- Vue
+    - vuex
+    - vue-router
+    - iview
+    - element
+    - vux
+    - vant
+- React
+- React-Native
+
+## before
 - jQuery
 - Bootstrap
 - require.js
 - jqPagination
+
+## tools
+- echarts
+- js-xlsx
