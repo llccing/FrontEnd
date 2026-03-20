97f33f743a5b6bbef8a99164964fd311cee7d412
llccing <llccing@users.noreply.github.com>
2026-03-20 07:48:58 +0000

    deploy: a914f0771153b0633b90d12fb6326935b5cba042

---
+++ assets/04-entry.html-BExHgS7d.js
@@ -0,0 +1,13 @@
+import{_ as n,c as a,a as e,o as i}from"./app-DfpXzhO6.js";const l={};function r(c,s){return i(),a("div",null,[...s[0]||(s[0]=[e(`<h1 id="从入口开始" tabindex="-1"><a class="header-anchor" href="#从入口开始"><span>从入口开始</span></a></h1><h2 id="重要文件路径" tabindex="-1"><a class="header-anchor" href="#重要文件路径"><span>重要文件路径</span></a></h2><div class="language-text line-numbers-mode" data-highlighter="prismjs" data-ext="text"><pre><code class="language-text"><span class="line">src/platforms/web/entry-runtime-with-compiler.js</span>
+<span class="line"></span>
+<span class="line">src/platforms/web/runtime/index.js</span>
+<span class="line"></span>
+<span class="line">src/core/index.js</span>
+<span class="line"></span>
+<span class="line">src/core/instance/index.js</span>
+<span class="line"></span>
+<span class="line">src/core/global-api/index.js</span>
+<span class="line"></span>
+<span class="line"></span>
+<span class="line"></span></code></pre><div class="line-numbers" aria-hidden="true" style="counter-reset:line-number 0;"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><h2 id="总结" tabindex="-1"><a class="header-anchor" href="#总结"><span>总结</span></a></h2><pre><code>它本质上就是一个用 Function 实现的 Class，然后它的原型 prototype 以及它本身都扩展了一系列的方法和属性
+</code></pre>`,5)])])}const d=n(l,[["render",r]]),p=JSON.parse('{"path":"/lib/vue-analysis/04-entry.html","title":"从入口开始","lang":"zh-CN","frontmatter":{},"git":{"updatedTime":1773992902000,"contributors":[{"name":"Rowan Liu","username":"","email":"lcf33123@gmail.com","commits":1}],"changelog":[{"hash":"a914f0771153b0633b90d12fb6326935b5cba042","time":1773992902000,"email":"lcf33123@gmail.com","author":"Rowan Liu","message":"Update README.md"}]},"filePathRelative":"lib/vue-analysis/04-entry.md"}');export{d as comp,p as data};
